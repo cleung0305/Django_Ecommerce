3c8513983e060746a4b01a6170460e373d24419d
Lok <loklok12614@gmail.com>
2020-11-25 16:47:05 -0500

    order-summary page

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ app_name = 'core'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ProductDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name="add-to-cart"),
     path('remove-from-cart/<slug>/', remove_from_cart, name="remove-from-cart"),
--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, View
 from django.template import RequestContext, Context
 from django.utils import timezone
@@ -17,6 +20,18 @@ class HomeView(ListView):
         context = self.get_context_data()
         return render(request, self.template_name, context)
 
+class OrderSummaryView(LoginRequiredMixin, View):
+    def get(self,*args, **kwargs):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            context = {
+                "object":order
+            }
+            return render(self.request, 'order_summary.html', context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, "You do not have an active order.")
+            return redirect("/")
+
 class ProductDetailView(DetailView):
     model = Item
     template_name = "product.html"
@@ -30,6 +45,7 @@ class CheckoutView(ListView):
     model = Order
     template_name = "checkout.html"
 
+@login_required
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_item, created = OrderItem.objects.get_or_create(
@@ -57,26 +73,31 @@ def add_to_cart(request, slug):
         messages.info(request, "This item was added to your cart.")
     return redirect("core:product", slug=slug)
 
+@login_required
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
-    order_queryset = Order.objects.filter(user=request.user, ordered=False)
-    if order_queryset.exists():
-        order = order_queryset[0]
-        #check if order item is in the order
-        if order.items.filter(item__slug=item.slug).exists():
-            order_item = OrderItem.objects.filter(
-                item=item,
-                user=request.user,
-                ordered=False
-            )[0]
-            order.items.remove(order_item)
-            messages.info(request, "This item was removed from your cart.")
-            return redirect("core:product", slug=slug)
-        else: 
-            #add a message saying the user does not have any order.
-            messages.info(request, "This item was not in your cart.")
-            return redirect("core:product", slug=slug)
-    else:
-        #add a message saying the user does not have any order.
-        messages.info(request, "You do not have any ordeer.")
+    if request.user.is_anonymous:
+        messages.info(request,"You are not logged in.")
         return redirect("core:product", slug=slug)
+    else:
+        order_queryset = Order.objects.filter(user=request.user, ordered=False)
+        if order_queryset.exists():
+            order = order_queryset[0]
+            #check if order item is in the order
+            if order.items.filter(item__slug=item.slug).exists():
+                order_item = OrderItem.objects.filter(
+                    item=item,
+                    user=request.user,
+                    ordered=False
+                )[0]
+                order.items.remove(order_item)
+                messages.info(request, "This item was removed from your cart.")
+                return redirect("core:product", slug=slug)
+            else: 
+                #add a message saying the user does not have any order.
+                messages.info(request, "This item was not in your cart.")
+                return redirect("core:product", slug=slug)
+        else:
+            #add a message saying the user does not have any order.
+            messages.info(request, "You do not have any ordeer.")
+            return redirect("core:product", slug=slug)
